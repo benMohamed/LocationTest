c6468a72ee366f2e8076be8622082f778ee4188d
benMohamed <bendnaiba@gmail.com>
2020-07-03 19:56:17 +0100

    Update AppDelegate.swift

--- LocationTest/AppDelegate.swift
+++ LocationTest/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
  
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
- 
+
     let locationManager:LocationManager = LocationManager()
     var window: UIWindow?
 
